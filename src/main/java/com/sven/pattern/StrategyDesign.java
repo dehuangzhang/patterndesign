8dca07426d1efc222b6feb1b2f89ea39401e04a2
dehuang_zhang@163.com <Sven.zhang1202?>
2017-10-09 14:44:02 +0800

    新增 策略模式

---
+++ src/main/java/com/sven/pattern/StrategyDesign.java
@@ -0,0 +1,49 @@
+package com.sven.pattern;
+
+/**
+ * @author sven.zhang
+ * @since 2017/9/28.
+ * @note 策略模式
+ * @note 定义了算法族，分别封装，让他们之间可以相互替换，让算法的变化独立于使用算法的客户
+ */
+public class StrategyDesign {
+
+    public static void main(String[] args) {
+        ContextStrategy strategy = new ContextStrategy(new FiveDiscountStrategy());
+        strategy.discount();
+    }
+}
+
+interface DiscountStrategy {
+
+    void discount();
+}
+
+class FiveDiscountStrategy implements DiscountStrategy {
+
+    @Override
+    public void discount() {
+        System.out.println("五折");
+    }
+}
+
+class oneDiscountStrategy implements DiscountStrategy {
+
+    @Override
+    public void discount() {
+        System.out.println("一折");
+    }
+}
+
+class ContextStrategy {
+
+    private DiscountStrategy discountStrategy;
+
+    public ContextStrategy(DiscountStrategy discountStrategy){
+        this.discountStrategy = discountStrategy;
+    }
+
+    public void discount() {
+        discountStrategy.discount();
+    }
+}
